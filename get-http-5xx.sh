01f396fc9f1c63e842b603c1dea568d38b9ada8a
tayalrishabh96 <rishabh@devtron.ai>
2023-11-16 01:15:50 +0530

    update script get-http-5xx.sh

--- get-http-5xx.sh
+++ get-http-5xx.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -s http://del-platform-monitoring-prometheus.monitoring:9090/api/v1/query -d 'query=sum(increase(orchestrator_http_requests_total{status=~"5.."}[2d])) by (path)'  | jq -r '.data.result[] | [.metric.path,(.value[-1] | tonumber | floor)] | @csv' > http-5xx.csv
 sed -i -e '1i"Path","Count"' http-5xx.csv
-python ses.py
+python3 ses.py
